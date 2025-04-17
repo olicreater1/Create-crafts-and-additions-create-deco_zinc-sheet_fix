6bd9d61567e63b0ebf66462a3d8888d611a8d93f
Oly Creator <75146579+olicreater1@users.noreply.github.com>
2025-04-17 13:12:09 -0400

    Create Zinc-sheets1.js

---
+++ Zinc-sheets1.js
@@ -0,0 +1,28 @@
+//makes create deco zinc sheets by mechanical saw
+event.custom({
+  "type":"create:cutting",
+  "ingredients":[
+    {
+    "item":"create:zinc_ingot"
+    }
+    ]
+  "results":[
+    {
+      "item":"createdeco:zinc_sheet"
+    }
+    ]
+})
+//makes create crafts and additions zinc sheets using mechanical press
+event.custom({
+  "type":"create:pressing",
+  "ingredients":[
+    {
+    "item":"create:zinc_ingot"
+    }
+    ]
+  "results":[
+    {
+      "item":"createcraftsandadditions:zinc_sheet"
+    }
+    ]
+})
